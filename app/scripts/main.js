f6d28f56d2ea526ca322719dc3bc6b6c3963ec91
marcus@mphasize.de <mpaeschke@mu-m-mpaeschke.ideo.com>
2014-10-22 11:27:34 +0200

    Simple D3 render of the ball position

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,46 @@
-console.log('\'Allo \'Allo!');
+var lemma = new Lemma('webclient', 'NoamDisplay');
+
+lemma.hears('ball', function(name, value) {
+	console.log('Ball: ', name, value);
+
+	svg.selectAll("circle")
+		.data([value])
+		.attr("cx", function (d) {
+      		return d.ballX;
+      	})
+      	.attr("cy", function (d) {
+      		return d.ballY;
+      	});
+  //update your page here.
+  //$("div#msg").append("<p>" + name + ":&nbsp;" + value +  "</p>");
+});
+
+lemma.begin('127.0.0.1', 8089);
+
+var ball = {
+	ballX: 0,
+	ballY: 0,
+	ballS: 0
+};
+
+var width = 960,
+    height = 500;
+
+var svg = d3.select("#pongtable").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var ball = svg.selectAll("circle")
+      .data([ball])
+    .enter().append("circle")
+      .attr("r", 5)
+      .attr("cx", function (d) {
+      	return d.ballX;
+      })
+      .attr("cy", function (d) {
+      	return d.ballY;
+      });
+
+
+
+
